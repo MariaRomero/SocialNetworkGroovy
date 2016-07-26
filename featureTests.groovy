7a3af9a52e7a21cfdcf45a4596b4dc3a5a8e3a94
MariaRomero <fantasysystems@gmail.com>
2016-07-26 08:22:00 +0100

    some changes to feature tests

--- featureTests.groovy
+++ featureTests.groovy
@@ -1,14 +1,33 @@
-import User
 
-spike = new User("Spike", new Post())
-leo = new User("leo", new Post())
-nikesh = new User("nikesh", new Post())
-spike.posts("Spike first message")
-nikesh.posts("Nikesh first message")
-spike.follow(nikesh)
-spike.read()
-leo.follow(spike)
-leo.follow(nikesh)
-leo.read()
+import spock.lang.*
+import spock.lang.specification
+
+// import User
+
+class featureTestSpec extends Specification {
+	def "Users can follow other users"() {
+		setup: "two user are created"
+			// def spike = new User("Spike", new Post());
+			// def nikesh = new User("Nikesh", new Post());
+			def a = 1
+			def b  =1
+		when:
+		 // spike.follow(nikesh)
+		 a = a + b
+		then:
+			// spike.following.size == 1
+			a == 2
+	}
+}
+// spike = new User("Spike", new Post())
+// leo = new User("leo", new Post())
+// nikesh = new User("nikesh", new Post())
+// spike.posts("Spike first message")
+// nikesh.posts("Nikesh first message")
+// spike.follow(nikesh)
+// spike.read()
+// leo.follow(spike)
+// leo.follow(nikesh)
+// println leo.read()
 
 
